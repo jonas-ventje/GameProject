18411de2661d1fac9373a3c9f32b074dde63aacb
Jonas Van Hoof <jonasvanhoof2003@gmail.com>
2022-11-10 19:36:02 +0100

    Add project files.

---
+++ GameProject/Content/Santa.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SharpDX.Direct2D1.Effects;
+using SharpDX.Direct3D9;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject.Content {
+    internal class Santa : IGameObject {
+        private Texture2D texture;
+        private List<Rectangle> idleFrames;
+        private List<Rectangle> walkingFrames;
+        private List<Rectangle> activeFrameList;
+        private int activeFrame = 0;
+        private double secondCounter;
+        private int fps = 15;
+        private Vector2 position;
+        private Vector2 speed;
+        private SpriteEffects spriteEffect;
+
+        public Santa(Texture2D texture, Vector2 speed) {
+            this.texture = texture;
+            position = new Vector2(0, 0);
+            spriteEffect = SpriteEffects.None;
+            idleFrames = new List<Rectangle>();
+            walkingFrames = new List<Rectangle>();
+
+            //find idle frames in spritesheet
+            for (int i = 17; i < 17 + 16; i++)
+                idleFrames.Add(spritePositionToRectangle(256, 176, 8, i));
+
+            //find walking frames in spritesheet
+            for (int i = 71; i < 71 + 13; i++)
+                walkingFrames.Add(spritePositionToRectangle(256, 176, 8, i));
+
+            activeFrameList = idleFrames;
+
+            this.speed = speed;
+
+
+        }
+        private Rectangle spritePositionToRectangle(int spriteWidth, int spriteHeigh, int numberHorizontal, int position) {
+            int startPixelHorizontal = spriteWidth * (position % numberHorizontal);
+            int startPixelVertical = spriteHeigh * (position / numberHorizontal);
+            return new Rectangle(startPixelHorizontal, startPixelVertical, spriteWidth, spriteHeigh);
+        }
+
+        private void move() {
+            KeyboardState state = Keyboard.GetState();
+            var direction = Vector2.Zero;
+            var prevFrameList = activeFrameList;
+            if (state.IsKeyDown(Keys.Left))
+                direction.X -= 1;
+            if (state.IsKeyDown(Keys.Right))
+                direction.X += 1;
+
+            //check which animation frame is required
+            if (direction == Vector2.Zero)
+                activeFrameList = idleFrames;
+            else if (direction.X != 0 && direction.Y == 0)
+            {
+                activeFrameList = walkingFrames;
+                if (direction.X < 0)
+                    spriteEffect = SpriteEffects.FlipHorizontally;
+                else
+                    spriteEffect = SpriteEffects.None;
+            }
+            //activeFrames must be set to 0 again when animation changes because it does not have equal numbers of frames
+            if (prevFrameList != activeFrameList)
+                activeFrame = 0;
+            direction *= speed;
+            position += direction;
+
+        }
+
+        public void draw(SpriteBatch spriteBatch) {
+            move();
+            spriteBatch.Draw(texture, position, activeFrameList[activeFrame], Color.White, 0f, new Vector2(0, 0), 1f, spriteEffect, 1f);
+        }
+
+        public void update(GameTime gameTime) {
+            secondCounter += gameTime.ElapsedGameTime.TotalSeconds;
+            if (secondCounter >= 1d / fps)
+            {
+                activeFrame++;
+                secondCounter = 0;
+            }
+            if (activeFrame >= activeFrameList.Count)
+                activeFrame = 0;
+        }
+    }
+}
---
+++ GameProject/Game1.cs
@@ -0,0 +1,59 @@
+﻿using GameProject.Content;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameProject
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Texture2D _santaTexture;
+
+        private Santa santa;
+
+        public Game1()
+        {
+
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            //_graphics.IsFullScreen = true;
+            _graphics.ApplyChanges();
+            base.Initialize();
+
+            santa = new Santa(_santaTexture, new Vector2(3,3));
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _santaTexture = Content.Load<Texture2D>("./images/santaClaus_small");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            santa.update(gameTime);
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            santa.draw(_spriteBatch);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
