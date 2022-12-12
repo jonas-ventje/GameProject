dab7cc16e584c0c9e614ff95e918c7c010967a87
Jonas Van Hoof <jonasvanhoof2003@gmail.com>
2022-12-12 15:38:32 +0100

    clean startup screen

--- GameProject/Content/Game/startScreen.cs
+++ GameProject/Content/Game/startScreen.cs
@@ -12,41 +12,35 @@ namespace GameProject.Content.Game {
     internal class StartScreen:IScreen {
         SpriteFont font;
         Texture2D buttonTexture;
-        Dictionary<string, Button> buttons;
+        Dictionary<Button, GameState> buttons;
         public StartScreen(ContentManager content) {
             buttonTexture = content.Load<Texture2D>("./images/button");
             font = content.Load<SpriteFont>("font/santa_christmas");
-            buttons= new Dictionary<string, Button>();
+            buttons= new Dictionary<Button, GameState>();
 
 
             int buttonPositionX = (Game1.virtualWidth - buttonTexture.Width) / 2;
-            buttons.Add( "lv1", new Button(buttonTexture, font, buttonPositionX, 800, "level 1"));
-            buttons.Add("lv2", new Button(buttonTexture, font, buttonPositionX, 1100, "level 2"));
-            buttons.Add("info", new Button(buttonTexture, font, buttonPositionX, 1400, "level info"));
+            buttons.Add(new Button(buttonTexture, font, buttonPositionX, 800, "level 1"), GameState.Level1);
+            buttons.Add(new Button(buttonTexture, font, buttonPositionX, 1100, "level 2"), GameState.Level2);
+            buttons.Add(new Button(buttonTexture, font, buttonPositionX, 1400, "level info"), GameState.Info);
         }
 
         public void Draw(SpriteBatch spriteBatch) {
             foreach (var button in buttons)
             {
-                button.Value.Draw(spriteBatch);
+                button.Key.Draw(spriteBatch);
             }
         }
 
         public GameState Update(GameTime gameTime) {
-            GameState nextGameState = GameState.StartScreen;
             foreach (var button in buttons)
             {
-                if (button.Value.Update() == true)
+                if (button.Key.Update() == true)
                 {
-                    if (button.Key == "lv1")
-                        nextGameState = GameState.Level1;
-                    if(button.Key == "lv2")
-                        nextGameState= GameState.Level2;
-                    if (button.Key == "info")
-                        nextGameState = GameState.Info;
+                    return button.Value;
                 }
             }
-            return nextGameState;
+            return GameState.StartScreen;
         }
     }
 }
